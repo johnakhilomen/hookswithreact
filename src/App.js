dfe9e1a4ee836258da2e38fc96162bdad5b12c2c
John Akhilomen <jonbasanti@gmail.com>
2020-10-22 16:52:59 -0500

    Added props to MainContent

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import  { Header } from "./Header";
 import { MainContent } from './MainContent';
 
 function App() {
+ const [employee] = useState({
+   name: "Joe Brown",
+   salary: 28938
+ }); 
   return (
     <div className="App">
       <Header></Header>
-      <MainContent></MainContent>
+      <MainContent emp = {employee}></MainContent>
      App
     </div>
   );
--- src/MainContent.js
+++ src/MainContent.js
@@ -4,12 +4,14 @@ const colors = ["blue", "red", "gray", "yellow"];
 let getRandomColor = () => {
     return colors[Math.floor(Math.random() * colors.length)]
 }
-export let MainContent = () => {
+export let MainContent = (props) => {
+    const {emp} = props;
     const [color, setColor] = useState("");
     useEffect(()=>{
         setColor(getRandomColor);
     }, [color]);
     return(<div>
         <h1 style={{color: getRandomColor()}}>Some text here!</h1>
+        <p>Brought from props: {JSON.stringify(emp)}</p>
     </div>)
 }
